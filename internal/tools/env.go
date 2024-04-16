78795324c0f1b71dadedc2738def5dd06f50605e
Tomkoid <tomaszierl@outlook.com>
2024-04-16 15:45:39 +0200

    fix(internal/tools/env.go): set default values before loading .env
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- internal/tools/env.go
+++ internal/tools/env.go
@@ -8,15 +8,15 @@ import (
 )
 
 func EnvLoad() {
+	// set default values
+	os.Setenv("PASSWORD", "esp32")
+	os.Setenv("NTFY_ROOM", "esp32-alerts")
+	os.Setenv("NTFY_TITLE", "Warning")
+	os.Setenv("NTFY_MSG", "Hotspot is down! Turn it back on.")
+
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println(".env: no env file doesn't exist")
-
-		// set default values
-		os.Setenv("PASSWORD", "esp32")
-		os.Setenv("NTFY_ROOM", "esp32-alerts")
-		os.Setenv("NTFY_TITLE", "Warning")
-		os.Setenv("NTFY_MSG", "Hotspot is down! Turn it back on.")
 
 		return
 	}
